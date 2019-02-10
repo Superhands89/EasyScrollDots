7607000465d695789098305a358a0688e19f4d41
Paul <pauldolby@live.co.uk>
2019-02-10 15:49:24 +0000

    fixed nav options and support for no jquery
    There are now 3 options on when calling the easyScrollDots function to allow for the existence of a sticky nav. This means that the scroll amount will now cater for the extra offset required when a fixed navigation bar is in the way.
    Also, the code now no longer relies on jQuery as there is now a vanilla fallback.

--- easyScrollDots.js
+++ easyScrollDots.js
@@ -1,6 +1,14 @@
-// custom scroll indicator
+/* Easy Scroll Dots --- https://github.com/Superhands89/EasyScrollDots
+* Quickly add anchor points throughout your web page/application and have navigational dots automatically appear
+* in a fixed position on the side of the page. This allows the user to click to scroll though sections of the page,
+* and it updates as they scroll.
+*
+* By Paul Dolby (Superhands89) 2019 --- https://github.com/Superhands89/
+*
+* MIT Licence - Free and unrestricted use.
+*/
 
-// standard dotsThrottle function
+// a throttle function
 function dotsThrottle(func, wait, options) {
     var context, args, result;
     var timeout = null;
@@ -33,10 +41,18 @@ function dotsThrottle(func, wait, options) {
     };
 };
 
+// global fixed nav options
+let dotFixedNavPresent = false;
+let dotFixedNavId = '';
+let dotFixedNavUp = false;
+
 // scroll indicator controller
-function easyScrollDots(fixedNav, fixedId, fixedUp) {
+function easyScrollDots(dotfixedOptions) {
     let scrollIndi = document.querySelectorAll('.scroll-indicator');
-    
+    dotfixedOptions.fixedNav === true ? dotFixedNavPresent = true : dotFixedNavPresent;
+    dotfixedOptions.fixedNavId === '' ? dotFixedNavId = false : dotFixedNavId = dotfixedOptions.fixedNavId;
+    dotfixedOptions.fixedNavUpward === true ? dotFixedNavUp = true : dotFixedNavUp;
+
     if (scrollIndi.length) {
         const scrollIndiTemplate = '<div class="scroll-indicator-controller"><span></span></div>';
         document.querySelector('body').lastElementChild.insertAdjacentHTML('afterend', scrollIndiTemplate);
@@ -75,7 +91,6 @@ function easyScrollDots(fixedNav, fixedId, fixedUp) {
             }; 
 
             Object.keys(indiScrollTopCollection).forEach(function (e) {
-               // console.log("propertyName = " + e + " --- collectionEntry = " + indiScrollTopCollection[e] + " --- minOffset = " + indiOffsetMin());
                 if (indiScrollTopCollection[e] == indiOffsetMin()) {
                     Array.prototype.forEach.call(scrollIndiControllerDots, function (el) {
                         if (el.classList.contains('active')) {
@@ -88,22 +103,92 @@ function easyScrollDots(fixedNav, fixedId, fixedUp) {
 
         }, 300);
 
-        $(window).on('scroll', handleIndiScroll);
+      window.onscroll = function() {
+          handleIndiScroll();
+      };
     }
 }
     
 function scrollIndiClicked(indiId) {
-    $('html, body').animate({
-        scrollTop: $('#' + indiId).offset().top
-    }, 700);
-    const scrollPos = document.body.getBoundingClientRect().top;
-
-    // option to extend upward travel distance for sticky navs that only appear when scrolling upward
-   /* setTimeout(function () {
-        if (document.body.getBoundingClientRect().top > scrollPos && window.matchMedia('(min-width: 768px)').matches) {
-            $('html, body').animate({
-                scrollTop: $('#' + indiId).offset().top - 108
-            }, 400);
+    if (window.jQuery) {
+        // if jquery is availble then we can use jquery animations
+        if (dotFixedNavPresent === true && dotFixedNavId.length) {
+            // there is a fixed nav and its id has been defined
+            const dotNavHeightElem = document.getElementById(dotFixedNavId);
+            const dotNavHeight = dotNavHeightElem.clientHeight;
+            const dotDocumentHtml = $('html, body');
+            const indiElement = $('#' + indiId);
+    
+            if (dotFixedNavUp === true) {
+                // fix nav on upward scroll only
+                dotDocumentHtml.animate({
+                    scrollTop: indiElement.offset().top
+                }, 700);
+                const scrollPos = document.body.getBoundingClientRect().top;
+                setTimeout(function () {
+                    if (document.body.getBoundingClientRect().top > scrollPos) {
+                        dotDocumentHtml.animate({
+                            scrollTop: indiElement.offset().top - dotNavHeight
+                        }, 400);
+                    }
+                }, 400);
+            }
+            else {
+                // fixed nav scroll
+                dotDocumentHtml.animate({
+                    scrollTop: indiElement.offset().top - dotNavHeight
+                }, 700);
+            }
         }
-    }, 400); */
+        else {
+            // normal scroll
+            $('html, body').animate({
+                scrollTop: $('#' + indiId).offset().top
+            }, 700);
+        }    
+    }
+    else {
+        // there is no jquery so we use vanilla scroll animations
+        if (dotFixedNavPresent === true && dotFixedNavId.length) {
+            // there is a fixed nav and its id has been defined
+            const dotNavHeightElem = document.getElementById(dotFixedNavId);
+            const dotNavHeight = dotNavHeightElem.clientHeight;
+            const indiElement = document.getElementById(indiId);
+    
+            if (dotFixedNavUp === true) {
+                // fix nav on upward scroll only
+                window.scrollTo({
+                    top: indiElement.offsetTop,
+                    left: 0,
+                    behavior: 'smooth'
+                });
+                const scrollPos = document.body.getBoundingClientRect().top;
+                setTimeout(function () {
+                    if (document.body.getBoundingClientRect().top > scrollPos) {
+                        window.scrollTo({
+                            top: indiElement.offsetTop - dotNavHeight,
+                            left: 0,
+                            behavior: 'smooth'
+                        });
+                    }
+                }, 400);
+            }
+            else {
+                // fixed nav scroll
+                window.scrollTo({
+                    top: indiElement.offsetTop - dotNavHeight,
+                    left: 0,
+                    behavior: 'smooth'
+                });
+            }
+        }
+        else {
+            // normal scroll
+            window.scrollTo({
+                top: document.getElementById(indiId).offsetTop,
+                left: 0,
+                behavior: 'smooth'
+            });
+        }
+    }
 }
